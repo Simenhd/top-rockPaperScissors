62d7e2a1d4474e149901be68e51ba5450ea7fd1e
Simenhd <simenhdale@gmail.com>
2025-03-19 21:34:17 +0100

    ui work

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const totalRounds = 5;
-const rounds = 0;
-const humanScore = 0;
-const computerScore = 0;
+let round = 0;
+let humanScore = 0;
+let computerScore = 0;
 
 const userSelectionBtns = document.querySelector(".game-grid");
 const result = document.querySelector(".gameResult");
@@ -27,18 +27,56 @@ function playRound(humanChoice, computerChoice) {
     };
 
     // return round results
-    return wins[humanChoice] === computerChoice ? 'Player' : 'Computer';
+    let roundResult = wins[humanChoice] === computerChoice ? 'Player' : 'Computer';
+
+    switch (roundResult) {
+    case 'Player':
+        humanScore += 1;
+        break;
+    case 'Computer':
+        computerScore +=1;
+        break;
+    default:
+        break;
+    };
+
+    playerScoreDisplay.textContent = humanScore;
+    computerScoreDisplay.textContent = computerScore;
+
+    
 };
+
+// Reset game
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    round = 0;
+    playerScoreDisplay.textContent = '0';
+    computerScoreDisplay.textContent = '0';
+    roundDisplay.textContent = '0';
+    
+}
 
 
 
 userSelectionBtns.addEventListener('click', (e) => {
-    let choices = ["rock", "paper", "scissors"];
-
+    // User selection
     const button = e.target.closest("button");
-    
+    if (!button) return;
     const humanChoice = button.id;
+
+    // Computer selection
     const computerChoice = getComputerChoice();
+
+    // Update round counter and display
+    round += 1
+    roundDisplay.textContent = round
+
+    playRound(humanChoice,computerChoice);
+
+
+    
+    
 
 
 });
